160d1991e55641a5ce2b0ba0db5610e39ea5a6b1
DhruvMarothi <51503387+DhruvMarothi@users.noreply.github.com>
2020-12-18 19:15:24 +0530

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
-import React, { Component, useState } from "react";
+import React, { Component } from "react";
 import "../styles/App.css";
 import Circle from "./circle";
 import Shape from "./shape";
+
 class App extends Component {
   render() {
     return (
